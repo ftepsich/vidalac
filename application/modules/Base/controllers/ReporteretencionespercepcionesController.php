351b304451826c789903562094f2a55516286daf
MrCrawled <fertepsi@gmail.com>
2017-09-05 14:46:18 -0300

    Cambios para Retenciones / Percepciones IB
    Cambios para Retenciones / Percepciones IB

--- application/modules/Base/controllers/ReporteretencionespercepcionesController.php
+++ application/modules/Base/controllers/ReporteretencionespercepcionesController.php
@@ -17,53 +17,57 @@ class Base_ReporteRetencionesPercepcionesController extends Rad_Window_Controlle
         $rq = $this->getRequest();
         $db = Zend_Registry::get('db');
         $param['modelo'] = $db->quote($rq->modelo, 'INTEGER');
-
-        // SIAGER Retenciones / Percepciones
+        // Utilizo LibroIVA solo para obtener el periodo.
+        $param['libro']  = $db->quote($rq->libro, 'INTEGER');
+        // Obtener Formato seleccionado.
+        $formato = ($rq->formato) ? $rq->formato : 'txt';
+        switch ($formato) {
+        case 'txt':
         if ($param['modelo'] == 1 || $param['modelo'] == 2) { // SIAGER Retenciones / Percepciones       
-            $modelo = new Base_Model_DbTable_ConceptosImpositivos();
-            $datos = $modelo->exportadorSIAGER();
+            $M_CI  = new Base_Model_DbTable_ConceptosImpositivos();
+            $datos = $M_CI->exportadorSIAGER($param['modelo'],$param['libro']);
             $exportador = new FileExport(FileExport::MODE_SEPARATOR);
             $exportador->setLineEnd("\r\n");
             $exportador->addAll($datos);
             $contenido = str_replace("\n\n", "", $exportador->getContent());
-            $reporte = $modelo->current();
-            $nombre = $reporte->Anio . "-" . str_pad($R->Mes, 2, '0', STR_PAD_LEFT) . "_SIAGER_" . ( ($param['modelo'] == 10) ? "Retenciones" : "Percepciones") . "_" . date('YmdHis') . ".txt";
+            $M_LI   = new Contable_Model_DbTable_LibrosIVA();
+            $R_LI   = $M_LI->find($param['libro'])->current();
+            $nombre = $R_LI->Anio . "-" . str_pad($R_LI->Mes, 2, '0', STR_PAD_LEFT) . "_SIAGER_" . ( ($param['modelo'] == 1) ? "Retenciones" : "Percepciones") . "_" . date('YmdHis') . ".txt";
             header("Content-disposition: attachment; filename=$nombre");
             header("Content-type: text/csv");
             echo $contenido;
         } else {
             throw new Exception("No se pudo generar el reporte");
         }
-
-
-        $M_L = new Base_Model_DbTable_ConceptosImpositivos();
-        $R_L = $M_L->find($param['libro'])->current();
-        if ($R_L) {
-            $tPeriodo = $R_L->Descripcion;
-        } else {
-            $tPeriodo = 'Desconocido';
-        }
-        $texto = "Concepto $tTipo periodo $tPeriodo";
-        $idLibro = $param['concepto'];
-        $formato = ($rq->formato) ? $rq->formato : 'pdf';
-
-
-        switch ($param['modelo']) {
+        break;
+        case 'pdf':
+        case 'xls':
+        if ($param['modelo'] == 1 || $param['modelo'] == 2) { // SIAGER Retenciones / Percepciones
+            $M_LI  = new Contable_Model_DbTable_LibrosIVA();
+            $R_LI = $M_LI->find($param['libro'])->current(); 
+            if ($R_LI) {
+               $periodo = $R_LI->Descripcion;
+            } else {
+               throw new Exception("No se pudo generar el reporte");
+            }
+            switch ($param['modelo']) {
             case 1:
-                $file = APPLICATION_PATH . "/../birt/Reports/Rep_Retenciones.rptdesign";
-                break;
+                 $file = APPLICATION_PATH . "/../birt/Reports/Siager_Retenciones.rptdesign";
+                 break;
             case 2:
-                $file = APPLICATION_PATH . "/../birt/Reports/Rep_Percepciones.rptdesign";
-                break;
+                 $file = APPLICATION_PATH . "/../birt/Reports/Siager_Percepciones.rptdesign";
+                 break;
+            }
+            $report->renderFromFile($file, $formato, array(
+                'TEXTO'   => "SIAGER Retenciones para el Periodo $periodo",
+                'PERIODO' => "$periodo"
+            ));
+            $NombreReporte = 'Reporte_' .  $R_LI->Anio . "-" . str_pad($R_LI->Mes, 2, '0', STR_PAD_LEFT) . '_SIAGER_' . ( ($param['modelo'] == 1) ? 'Retenciones' : 'Percepciones') . '_' . date('YmdHis');
+            $report->sendStream($NombreReporte);
+        } else {
+            throw new Exception("No se pudo generar el reporte");
         }
-        $where = " WHERE " . $this->buildWhere($param);
-        $report->renderFromFile($file, $formato, array(
-            'TEXTO' => $texto,
-            'WHERE' => $where,
-            'IDLIBRO' => $idLibro
-        ));
-        $nombreRep = str_replace(array(" ", "/"), array("_", "-"), $texto);
-        $NombreReporte = 'Reporte_' . $nombreRep . "_" . date('YmdHis');
-        $report->sendStream($NombreReporte);
+        break;
+        }
     }
 }
