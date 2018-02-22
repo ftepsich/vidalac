1c315122e331b8776dada6ca8be23279cbcf7c8f
MrCrawled <fertepsi@gmail.com>
2018-02-22 14:51:27 -0300

    Update CuentasCorrientes.php

--- application/modules/Contable/models/DbTable/CuentasCorrientes.php
+++ application/modules/Contable/models/DbTable/CuentasCorrientes.php
@@ -275,7 +275,7 @@ class Contable_Model_DbTable_CuentasCorrientes extends Rad_Db_Table {
         
         public function fetchCuentaCorrienteComoProveedor($where = null, $order = null, $count = null, $offset = null) {
         $condicion = "(
-             (TiposDeComprobantes.Grupo IN (1,8,9,13,20) AND Comprobantes.EsCliente = 0)
+             (TiposDeComprobantes.Grupo IN (1,8,9,13,15,20) AND Comprobantes.EsCliente = 0)
              OR (TiposDeComprobantes.Grupo in (6,7,12) AND Comprobantes.EsProveedor = 1) 
              OR (fNumeroCompleto(CuentasCorrientes.Comprobante,'S') COLLATE utf8_general_ci LIKE '%Saldo s/Orden de Pago%') 
              )";
