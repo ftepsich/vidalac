6066448a236661ad5dacbac10f10c407d484cad2
Fernando Tepsich <fertepsi@gmail.com>
2018-03-28 17:52:15 -0300

    Hashing en clave de usuarios.

--- application/models/DbTable/Usuarios.php
+++ application/models/DbTable/Usuarios.php
@@ -28,7 +28,51 @@ class Model_DbTable_Usuarios extends Rad_Db_Table_SemiReferencial
             'refColumns'        => 'Id',
         )   
     );
-    
-    //protected $_dependentTables = array('Model_DbTable_UsuariosConfiguraciones'); 
+
+    /**
+     * Inserta un Registro en la Tabla de Usuarios
+     *
+     * @param array $data
+     * @return mixed
+     */
+    public function insert($data)
+    {
+
+        $this->_db->beginTransaction();
+        try {
+            $data['ClaveHash'] = password_hash($data['Clave'], PASSWORD_BCRYPT, array('salt' => '754CC93A968B7F919C1C6477457F3'));
+            Rad_Log::user("Nuevo Usuario :".$data['Nombre']." con Clave Hash : ". $data['ClaveHash']);
+            $id = Rad_Db_Table::insert($data);
+            $this->_db->commit();
+            return $id;
+        } catch (Exception $e) {
+            $this->_db->rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Updateo un Registro en la Tabla de Usuarios
+     *
+     * @param array $data
+     * @param mixwd $where
+     * @return mixed
+     */
+    public function update($data, $where)
+    {
+
+        $this->_db->beginTransaction();
+        try {
+            if (isset($data['Clave'])) {
+                $data['ClaveHash'] = password_hash($data['Clave'], PASSWORD_BCRYPT, array('salt' => '754CC93A968B7F919C1C6477457F3'));
+                Rad_Log::user("Cambió de Clave en Usuario :".$data['Nombre']." ahora con Clave Hash :". $data['ClaveHash']);
+            }
+            Rad_Db_Table::update($data, $where);
+            $this->_db->commit();
+        } catch (Exception $e) {
+            $this->_db->rollBack();
+            throw $e;
+        }
+    }
     
 }
