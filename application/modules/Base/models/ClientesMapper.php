66757cdbb55c3d0cb367e94a21e8e57253e8dfbb
MrCrawled <fertepsi@gmail.com>
2018-05-03 13:06:04 -0300

    Ajuste para gestionar avisos a usuarios
    Ajuste para gestionar avisos a usuarios

---
+++ application/modules/Base/models/ClientesMapper.php
@@ -0,0 +1,18 @@
+<?php
+
+class Base_Model_ClientesMapper extends Rad_Mapper
+{
+    protected $_class = 'Base_Model_DbTable_Clientes';
+
+    public function getIBProximosVencimientosCM05($persona)
+    {
+        $db = Zend_Registry::get('db');
+        return $db->fetchOne("SELECT COUNT(Id) FROM personasingresosbrutos WHERE Persona = $persona AND FechaVencimientoCM05 IS NOT NULL AND FechaVencimientoCM05 < CURDATE() AND FechaBaja IS NULL");
+    }
+
+    public function getIBItems($persona)
+    {
+        $db = Zend_Registry::get('db');
+        return $db->fetchOne("SELECT COUNT(Id) FROM personasingresosbrutos WHERE Persona = $persona AND FechaBaja IS NULL");
+    }
+}
