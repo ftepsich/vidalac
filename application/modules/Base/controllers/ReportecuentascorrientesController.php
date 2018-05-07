e64f7baa1c7a0ca4312583d27680dc80e56986a3
MrCrawled <fertepsi@gmail.com>
2018-05-06 21:26:10 -0300

    Ajuste para evitar traer diferencias menores a $1
    Ajuste para evitar traer diferencias menores a $1

--- application/modules/Base/controllers/ReportecuentascorrientesController.php
+++ application/modules/Base/controllers/ReportecuentascorrientesController.php
@@ -47,9 +47,9 @@ class Base_ReporteCuentasCorrientesController extends Rad_Window_Controller_Acti
         if ($param['ocultarCeros'] == 1) {
 
               if ( $param['modelo'] != 3 ) {
-                   $where[] = "fPersona_CuentaCorriente_A_Fecha(P.Id,".$param['fecha'].",".$param['modelo'].",".$param['tipo'].") <> 0";
+                   $where[] = "fPersona_CuentaCorriente_A_Fecha(P.Id,".$param['fecha'].",".$param['modelo'].",".$param['tipo'].")  NOT BETWEEN -1 AND 1";
               } else {
-                   $where[] = "IFNULL(fComprobante_Monto_Disponible(C.Id),0) <> 0";
+                   $where[] = "IFNULL(fComprobante_Monto_Disponible(C.Id),0) NOT BETWEEN -1 AND 1";
               }
         }
         
