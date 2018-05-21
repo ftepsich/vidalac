a291eeeb725ff6a8368eb762708f432ef7ed4285
Fernando Tepsich <fertepsi@gmail.com>
2018-05-21 14:22:06 -0300

    Se subieron los cambios correspondientes a la modificacion del formulario de Clientes

---
+++ application/modules/Base/models/DbTable/TiposFormasDePagos.php
@@ -0,0 +1,14 @@
+<?php
+require_once('Rad/Db/Table.php');
+
+class Base_Model_DbTable_TiposFormasDePagos extends Rad_Db_Table
+{
+	// Tabla mapeada
+	protected $_name = "TiposFormasDePagos";
+
+	// Relaciones
+    protected $_referenceMap    = array(
+            );
+	
+	
+}
