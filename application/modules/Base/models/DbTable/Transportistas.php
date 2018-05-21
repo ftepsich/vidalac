c98729d4118ec62ae814c95ed31e525419f9f5ca
Fernando Tepsich <fertepsi@gmail.com>
2018-05-21 16:06:17 -0300

    Actualizacion de codigo

--- application/modules/Base/models/DbTable/Transportistas.php
+++ application/modules/Base/models/DbTable/Transportistas.php
@@ -6,28 +6,6 @@ class Base_Model_DbTable_Transportistas extends Base_Model_DbTable_Personas
     protected $_name = "Personas";
     Protected $_sort = array("RazonSocial asc");
 	protected $_permanentValues = array('EsTransporte' => 1);
-    // Inicio  protected $_referenceMap --------------------------------------------------------------------------
-    protected $_referenceMap = array(
-        'ModalidadesIVA' => array(
-            'columns' => 'ModalidadIVA',
-            'refTableClass' => 'Base_Model_DbTable_ModalidadesIVA',
-            'refJoinColumns' => array("Descripcion"), 	// De esta relacion queremos traer estos campos por JOIN
-            'comboBox' => true, 						
-            'comboSource' => 'datagateway/combolist',
-            'refTable' => 'ModalidadesIVA',
-            'refColumns' => 'Id'
-        ),
-        'TiposDeInscripcionesGanancias' => array(
-            'columns' => 'ModalidadGanancia',
-            'refTableClass' => 'Base_Model_DbTable_TiposDeInscripcionesGanancias',
-            'refJoinColumns' => array("Descripcion"), 	// De esta relacion queremos traer estos campos por JOIN
-            'comboBox' => true, 						
-            'comboSource' => 'datagateway/combolist',
-            'refTable' => 'TiposDeInscripcionesGanancias',
-            'refColumns' => 'Id'
-        )
-    );
-
 }
 
 ?>
