52b97c406cc24c65dea030ee5e55d4678d7c065a
MrCrawled <fertepsi@gmail.com>
2019-02-06 19:15:20 -0300

    Se agrega relacion en OP Facturas Ordenes Compra
    Se agrega relacion en OP Facturas Ordenes Compra

--- application/modules/Facturacion/controllers/OrdenesdecomprasController.php
+++ application/modules/Facturacion/controllers/OrdenesdecomprasController.php
@@ -73,5 +73,56 @@ class Facturacion_OrdenesDeComprasController extends Rad_Window_Controller_Actio
         $html .= '</div></div>';
         echo $html;
     }
+    
+    public function getdetalleestado2Action()
+    {
+        $this->_helper->viewRenderer->setNoRender(true);
+
+        $id = $this->getRequest()->Id;
+
+        $M_CR = new Facturacion_Model_DbTable_ComprobantesRelacionados;
+
+        $sql = "SELECT c_oc.numero as numero,
+                       a_oc.descripcion as concepto,
+                       cd_oc.cantidad as cantidad ,
+                       cd_oc.preciounitario as precio,
+                       round(cd_oc.cantidad * cd_oc.preciounitario,4) as monto,
+                       c_oc.observacionesimpresas as observaciones
+                FROM comprobantes c_oc
+                JOIN comprobantesdetalles cd_oc ON cd_oc.comprobante = c_oc.id
+                JOIN articulos a_oc             ON a_oc.id = cd_oc.articulo
+                JOIN tiposdecomprobantes tc_oc  ON tc_oc.id = c_oc.tipodecomprobante
+                WHERE tc_oc.grupo = 5
+                  AND EXISTS (  SELECT 1
+                                FROM comprobantes c_r
+                                JOIN tiposdecomprobantes tc_r  ON tc_r.id = c_r.tipodecomprobante
+                                JOIN comprobantesrelacionados cr_oc_r ON cr_oc_r.ComprobantePadre = c_r.id
+                                WHERE cr_oc_r.ComprobanteHijo = c_oc.id
+                                  AND tc_r.grupo IN (4,10)
+                                  AND EXISTS (  SELECT 1
+                                                FROM comprobantes c_fc
+                                                JOIN tiposdecomprobantes tc_fc        ON tc_fc.id = c_fc.tipodecomprobante
+                                                JOIN comprobantesrelacionados cr_r_fc ON cr_r_fc.ComprobantePadre = c_fc.id
+                                                WHERE cr_r_fc.ComprobanteHijo  = c_r.id
+                                                  AND tc_fc.grupo IN (1) AND c_fc.Id = '".$id."') )";
+        
+        $db = $M_CR->getAdapter();
+        $R_CR = $db->fetchAll($sql);
+
+        if(!empty($R_CR)){
+            $count = 1;
+            foreach ($R_CR as $row) {
+                if ($count == 1) {
+                   $html = '<div class="panelDeDetalles panelDetalleAzul"><h3>Detalle Orden de Compra Nro. '.$row['numero'].'</h3><div class="detalle"><div class="col3 bborder">Cantidad</div><div class="col3 bborder">Precio Unitario</div><div class="col3 bborder">Monto</div>';
+                }
+                $html .= '<div class="listrow">'.$row['concepto'].'<div class="col3 bborder"><span>'.$row['cantidad'].'</span></div><div class="col3 bborder"><span>'.$row['precio'].'</span></div><div class="col3 bborder"><span>'.$row['monto'].'</span></div></div><div class="listrow">'.$row['observaciones'].'</div>';
+                $count += 1;
+            }
+            $html .= '</div></div>';
+        } else {
+            $html = '<div class="panelDeDetalles panelDetalleAzul"><h3>Sin Orden de Compra Asociada</h3></div>';
+        }
+        echo $html;
+    }
 
 }
