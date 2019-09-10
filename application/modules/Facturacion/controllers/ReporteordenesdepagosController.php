8ff40d7579d14ff0cf64aa79c40f0677d16c5b8c
MrCrawled <fertepsi@gmail.com>
2019-09-10 20:33:00 -0300

    Create ReporteordenesdepagosController.php

---
+++ application/modules/Facturacion/controllers/ReporteordenesdepagosController.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Facturacion_ReporteOrdenesDePagosController
+ *
+ * 
+ *
+ * 
+ * @package Aplicacion
+ * 
+ * @class Facturacion_ReporteOrdenesDePagosController
+ * @extends Rad_Window_Controller_Action
+ */
+class Facturacion_ReporteOrdenesDePagosController extends Rad_Window_Controller_Action
+{
+    protected $title = 'Reporte de Ordenes de Pagos';
+
+    public function initWindow()
+    {
+    }
+    
+    protected function buildWhere($param)
+    {
+        $where = array();
+        if ($param['fechadesde'])   $where[] = "ComprobantePadre_FechaEmision >= '".$param['fechadesde']."'";
+        if ($param['fechahasta'])   $where[] = "ComprobantePadre_FechaEmision <= '".$param['fechahasta']."'";
+        if ($param['persona'])      $where[] = "Persona = ".$param['persona'];
+        $where = implode (' and ', $where);
+        return $where;
+    }
+
+    public function verreporteAction ()
+    {
+        $this->_helper->viewRenderer->setNoRender(true);
+        
+        $texto = 'Reporte de Ordenes de Pagos'. $texto;
+
+        $report = new Rad_BirtEngine();
+
+        $rq = $this->getRequest();
+
+        if ($rq->persona && $rq->persona != "undefined") {
+           $param['persona']  = $rq->persona;
+           $texto .= " de una persona en particular";
+        }
+        
+        if ($rq->fechadesde) {
+           $param['fechadesde'] = $rq->fechadesde;
+           $texto .= " desde ".date('d/m/Y',strtotime($rq->fechadesde));
+        }
+
+        if ($rq->fechahasta) {
+           $param['fechahasta'] = $rq->fechahasta;
+           $texto .= " hasta ".date('d/m/Y',strtotime($rq->fechahasta));
+        }
+
+        $formato = ($rq->formato) ? $rq->formato : 'pdf';
+
+        $file  = APPLICATION_PATH . '/../birt/Reports/Reporte_OrdenesDePagos.rptdesign';
+
+        $where = $this->buildWhere($param);
+
+        //Rad_Log::debug($where);
+
+        $report->renderFromFile($file, $formato, array(
+            'WHERE' => " WHERE ".$where,
+            'TEXTO' =>  $texto
+        ));
+        
+        $nombreRep      = str_replace(  array(" ","/"), array("_","-") , $texto);
+        $NombreReporte  = $nombreRep."___".date('YmdHis');
+        
+        $report->sendStream($NombreReporte);
+    }
+}
